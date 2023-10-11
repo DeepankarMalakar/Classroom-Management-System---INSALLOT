05390529e7f8d7c3972381575d29e9fee5f027a2
DeepankarMalakar <deepankarmalakar007@gmail.com>
2023-10-11 12:42:30 +0530

    Project programs

---
+++ Sign-in.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign-in form</title>
+    <link rel="stylesheet" href="Sign-in.css">
+</head>
+
+<body>
+    <div class="wrapper-login">
+        <div class="container">
+            <div class="col-left">
+                <div class="login-text">
+                    <h2>WELCOME</h2>
+                    <p>Create an account</p>
+                    <a href="Login3.html" class="btn">Sign Up</a>
+                    <p> <img src="INS (4.png"></p>
+                </div>
+            </div>
+            <div class="col-right">
+                <div class="login-form">
+                    <h2>Login</h2>
+                    <form action="">
+                        <p>
+                            <label>Username or Email address<span>*</span></label>
+                            <input type="text" placeholder=" Enter your Username or email address" required>
+                        </p>
+
+                        <p>
+                            <label>Password<span>*</span></label>
+                            <input type="password" placeholder="Enter your password" required>
+                        </p>
+
+                        <p>
+                            <input type="submit" value="Sign In">
+                        </p>
+
+                        <p>
+                            <a href="">Forget Password?</a>
+                        </p>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Sign-up.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign up</title>
+    <link rel="stylesheet" href="Sign-up.css" />
+</head>
+
+<body>
+    <div class="wrapper-signup">
+        <div class="container">
+            <div class="signup-text">
+                <h2>
+                    <img src="inspiria-logo-horizontal.png"  class ="center">Sign Up 
+                </h2>
+                <br />
+                <sub id="create">Please fill in this form to create an account!</sub>
+                <br>
+                <br>
+                <form action=""></form>
+                <input type="text" placeholder="First Name" required />
+                <br>
+                <br>
+                <input type="text" placeholder="Last Name" required />
+                <br>
+                <br>
+                <input type="email" placeholder="Email" required />
+                <br>
+                <br>
+                <input type="password" placeholder="Password" required />
+                <br>
+                <br>
+                <input type="password" placeholder="Confirm Password" required />
+                <br>
+                <br>
+                <input type="checkbox" name="terms" id="terms">
+                <label for="terms">I accept the <a href="#">Terms of Use</a> & <a href="#">Privacy Policy</a></label>
+                <br>
+                <br>
+                <p>
+                    <input type="submit" value="Sign Up">
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
